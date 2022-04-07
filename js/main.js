6e53744b4d47c0eabe7aa7a378e83d975cafc9e4
irnag <power700996@gmail.com>
2022-04-07 12:18:05 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// 현재시간 표시 기능
+document.getElementById("time").innerHTML = new Date().toDateString();
+
+function pink() {
+  document.body.style.backgroundColor = "pink";
+}
+function lightblue() {
+  document.body.style.backgroundColor = "lightblue";
+}
+function yellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+function reset() {
+  document.body.style.backgroundColor = "linen";
+}
+
+function showhtml() {
+  document/getElementById("fig").src = "img/ html.png";
+  document.getElementById("desc").innerHTML = "CSS는 HTML의 머시기"
+}
